7147f1363bb8fd032c4e048bab3823eeb8dbee2a
zhongshao <zhongshao0122@gmail.com>
2024-12-17 22:11:44 +0800

    [update] 首次更新java

---
+++ src/main/java/com/nan/javaonlinetradingsystem/JavaOnlineTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nan.javaonlinetradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOnlineTradingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOnlineTradingSystemApplication.class, args);
+    }
+
+}
